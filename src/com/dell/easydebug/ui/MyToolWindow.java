ffea8b6743f81454ac023ba40d461ffb763ac8d2
Nitzan Zamir <Nitzan.Zamir@emc.com>
2019-02-06 16:51:06 +0200

    [UI] - basic view for remote debug

--- src/com/dell/easydebug/ui/MyToolWindow.java
+++ src/com/dell/easydebug/ui/MyToolWindow.java
@@ -5,15 +5,55 @@ import com.intellij.openapi.wm.ToolWindow;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Calendar;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class MyToolWindow {
 
 	private JButton hideToolWindowButton;
 	private JPanel myToolWindowContent;
+	private JTabbedPane tabbedPane;
+	private JFormattedTextField rpaIpTextField;
+	private JFormattedTextField rpaUserTextField;
+	private JPasswordField rpaPasswordField;
+	private JButton debugButton;
+
+	private String rpaIp = "127.0.0.1";
+	private String rpaUser = "root";
+	private String rpaPass = "";
 
 	MyToolWindow(ToolWindow toolWindow) {
-		hideToolWindowButton.addActionListener(event -> toolWindow.hide(null));
+
+		rpaIpTextField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				super.keyReleased(e);
+				rpaIp = rpaIpTextField.getText();
+			}
+		});
+		rpaUserTextField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				super.keyReleased(e);
+				rpaUser = rpaUserTextField.getText();
+			}
+		});
+		rpaPasswordField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				super.keyReleased(e);
+				rpaPass = String.valueOf(rpaPasswordField.getPassword());
+			}
+		});
+
+		debugButton.addActionListener(event -> excuteRemoteDebug());
+
+	}
+
+	private void excuteRemoteDebug() {
+		System.out.println("got remote debug request with params : " +
+				"RPA IP=" + rpaIp + ", User=" + rpaUser + ", Pass=" + rpaPass);
+
 	}
 
 	JPanel getContent() {
