99cc058a16a845b02fe6571a469fe3a868a48595
Itai Martsiano <itai.marts@gmail.com>
2019-02-06 18:42:17 +0200

    add ssh session

---
+++ src/com/dell/easydebug/utils/ssh/SshSession.java
@@ -0,0 +1,75 @@
+package com.dell.easydebug.utils.ssh;
+
+import com.jcraft.jsch.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SshSession implements AutoCloseable {
+
+    private static final int BUFFER_SIZE = 1024;
+    private final Session session;
+
+    private final static String KEY_CHECKING = "StrictHostKeyChecking";
+    private final static int THREAD_SLEEP_IN_TIME_MILLIS = 1000;
+
+
+    SshSession(String host, String password) {
+        JSch jsch = new JSch();
+        try {
+            session = initSession(host, password, jsch);
+        } catch (JSchException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    private Session initSession(String host, String password, JSch jsch) throws JSchException {
+        final int atIndex = host.indexOf('@');
+        final String user = host.substring(0, atIndex);
+        final String server = host.substring(atIndex + 1);
+        Session session = jsch.getSession(user, server, 22);
+        session.setConfig(KEY_CHECKING, "no");
+        session.setPassword(password);
+        return session;
+    }
+
+    public void connect() throws JSchException {
+        session.connect();
+    }
+
+    public void connect(int connectTimeout) throws JSchException {
+        session.connect(connectTimeout);
+    }
+
+    int getOutput(ChannelExec channelExec, InputStream in) throws IOException {
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int exitStatus;
+        while (true) {
+            while (in.available() > 0) {
+                int i = in.read(buffer);
+                if (i < 0) break;
+//                log.debug(new String(buffer, 0, i));
+            }
+            if (channelExec.isClosed()) {
+                if (in.available() > 0) continue;
+                exitStatus = channelExec.getExitStatus();
+//                log.debug("exit-status: " + exitStatus);
+                break;
+            }
+            try {
+                Thread.sleep(THREAD_SLEEP_IN_TIME_MILLIS);
+            } catch (InterruptedException ignored) {
+            }
+        }
+        return exitStatus;
+    }
+
+    @Override
+    public void close() throws RuntimeException {
+        session.disconnect();
+    }
+}
